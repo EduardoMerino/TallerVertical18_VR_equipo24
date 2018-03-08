8a59b67e0189315f82c2cf5b3279e23b7041bbf4
alberto <beto.he@outlook.com>
2018-03-08 12:07:12 -0600

    upload version jueves

---
+++ Assets/Audio_manager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Audio_manager : MonoBehaviour {
+	public bool IsPlay = false;
+	private bool IsQuestion = true;
+	private AudioSource aSrc;
+	private int Question;
+	public AudioClip intro;
+	public AudioClip [] preguntas;
+	private int count=0;
+	private GameObject[] pieces;
+	private Animator anim;
+	private PlayableDirector timelines;
+
+
+
+
+	// Use this for initialization
+	void Awake()
+	{
+		this.timelines=this.GetComponentInChildren<PlayableDirector>();
+	}
+	void Start () {
+		this.anim = this.GetComponentInChildren<Animator> ();
+		//this.preguntas= new AudioClip[4][];
+		this.pieces = GameObject.FindGameObjectsWithTag("Pieces");
+		this.aSrc = this.GetComponent<AudioSource> ();
+	}
+
+
+	public IEnumerator start()
+	{
+		
+		this.timelines.Play();
+		aSrc.clip = intro;
+		aSrc.Play();
+		anim.SetTrigger ("Inicio");
+		yield return new WaitForSeconds(intro.length);
+
+		foreach(GameObject t in pieces)
+		{
+			t.GetComponent<pieceSelectAlt>().StartScene=true;
+		}
+		aSrc.clip = preguntas [this.count];
+		aSrc.Play ();
+		yield return new WaitForSeconds(preguntas [this.count].length);
+		this.timelines.Stop ();
+	}
+
+	public void PlayAudio(int State,AudioClip clip, Texture2D tex)
+	{
+		StopAllCoroutines ();
+		if (State == 0)
+		{
+			StartCoroutine (start ());
+			return;
+		} 
+		if (State == 1) 
+		{
+			IsQuestion = true;
+			count++;
+		} 
+		StartCoroutine (Audio (clip));
+	}
+
+	private IEnumerator Audio(AudioClip clip)
+	{
+		aSrc.clip = clip;
+		aSrc.Play();
+		yield return new WaitForSeconds(clip.length);
+		if (IsQuestion && count<preguntas.Length) 
+		{
+			aSrc.clip = preguntas [this.count];
+			aSrc.Play ();
+			yield return new WaitForSeconds(preguntas [this.count].length);
+			IsQuestion = false;
+		}
+	}
+}
---
+++ Assets/Scripts/pieceSelectAlt.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class pieceSelectAlt : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public float gaze_time = 2f;
+	public Transform target;
+	public float speed = 3.5f;
+	public float rotate_speed = 25f;
+	public bool is_correct = true;
+	public bool is_activated = false;
+	public int appear_on_question;
+	public Transform start_position;
+	public bool StartScene = false;
+	public AudioClip Audio;
+	public Texture2D history;
+
+	private Audio_manager AudioM;
+	private float rim=2.0f;
+	private MeshRenderer meshR;
+	private float timer;
+	private GameObject load_bar;
+	private bool gazed_at = false;
+	private bool is_moving = false;
+	private bool is_bar_active = true;
+	private bool call_activate_others_once = true;
+	private bool is_on_start_position = false;
+	private pieceManager my_piece_manager;
+	private float my_distance = 0f;
+	private float distance_from_start = 0f;
+	private float x_angle;
+
+	// Use this for initialization
+	void Start () {
+		my_piece_manager =  GameObject.Find("PiecesManagerObj").GetComponent<pieceManager>();
+		this.load_bar = this.transform.GetChild (0).gameObject;
+		this.load_bar.SetActive (false);
+		this.is_activated = false;
+		this.meshR = this.GetComponentInChildren<MeshRenderer> ();
+		this.AudioM = GameObject.FindGameObjectWithTag ("Audio").GetComponent<Audio_manager> ();
+		//this.gameObject.SetActive (false);
+	}
+
+
+	public void OnPointerEnter(PointerEventData data){
+		if (this.is_activated) {
+			this.gazed_at = true;
+			meshR.material.SetFloat ("_True",1.0f);
+			StartCoroutine (desapear ());
+		}
+	}
+
+	public void OnPointerExit(PointerEventData data){
+		this.gazed_at = false;
+		meshR.material.SetFloat ("_True",0.0f);
+		meshR.material.SetFloat ("_RimPower",2.0f);
+		this.rim = 2.0f;
+		StopAllCoroutines ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		//Debug.Log ("my current question is: " + this.my_piece_manager.current_question);
+		if (this.appear_on_question == this.my_piece_manager.current_question && StartScene ) {
+			//this.gameObject.SetActive (true);
+
+			my_distance = Vector3.Distance (this.target.position, this.transform.position);
+			distance_from_start = Vector3.Distance (this.start_position.position, this.transform.position);
+			Transform child = load_bar.transform;
+			//move piece to start position:
+			if (!this.is_on_start_position) {
+				float step = (speed + 4f) * Time.deltaTime;
+				this.transform.position = Vector3.MoveTowards (this.transform.position, this.start_position.position, step);
+				if (this.distance_from_start <= 0.1f) {
+					this.is_activated = true;
+					this.is_on_start_position = true;
+				} else {
+					this.is_activated = false;
+				}
+			}
+
+			//Handle bar behaviour: 
+			if (this.gazed_at && this.is_bar_active) {
+				timer += Time.deltaTime;
+				load_bar.SetActive (true);
+
+				Vector3 newScale = new Vector3 ((timer + 1) / gaze_time, child.localScale.y, child.localScale.z);
+				Vector3 newPosition = new Vector3 ((timer / gaze_time) / 2, child.localPosition.y, child.localPosition.z);
+
+				child.localScale = newScale;
+				child.localPosition = newPosition;
+				if (timer >= gaze_time) {
+					//ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+					myPointerDown ();
+					timer = 0f;
+				}
+			} else {
+				timer = 0f;
+				load_bar.SetActive (false);
+			}
+
+			//Move piece to board: 
+			if (this.is_moving) {
+				float step = speed * Time.deltaTime;
+				this.transform.position = Vector3.MoveTowards (this.transform.position, this.target.position, step);
+
+				this.x_angle += Time.deltaTime * this.rotate_speed;
+				this.transform.rotation = Quaternion.Euler (-this.x_angle, 0, 0);
+
+				//Reached location at board:
+				if (this.my_distance <= 0.1f) {
+					this.transform.rotation = Quaternion.Euler (-90, 0, 0);
+					Debug.Log ("LLegué a mi destino");
+					if (this.call_activate_others_once) {
+						this.activateOthersOnce ();
+					}
+
+
+					if (this.is_correct) {
+						//Trigger correct answer sequence.
+						AudioM.PlayAudio(1,this.Audio,this.history);
+						//play audio
+						//display image.
+						//when audio ends:
+						this.my_piece_manager.activateAll ();
+						this.my_piece_manager.changeQuestion ();
+
+						//New question and set of pieces appears
+					} else {
+						//Trigger incorrect answer sequence. 
+						//play audio
+						AudioM.PlayAudio(2,this.Audio,this.history);
+						//display image
+						//when audio ends:
+						this.my_piece_manager.activateAll ();
+						this.is_bar_active = true;
+
+						Destroy (this.gameObject);
+					}
+					this.is_moving = false;
+				}
+			}
+
+
+
+
+		} else if((this.my_piece_manager.current_question > this.appear_on_question)
+			&& !this.is_correct){
+			Destroy (this.gameObject);
+		}
+	}//END UPDATE.
+
+	private void activateOthersOnce(){
+		this.call_activate_others_once = false;
+		this.my_piece_manager.activateAll ();
+		
+	}
+
+	private void myPointerDown(){
+		//SceneManager.LoadScene(1);
+		Debug.Log("Pointer Down");
+		this.is_moving = true;
+		this.is_bar_active = false;
+		this.my_piece_manager.deactivateOthers (this);
+	}
+	private IEnumerator desapear ()
+	{
+		while (this.rim > 0.0f) 
+		{
+			meshR.material.SetFloat ("_RimPower", this.rim);
+			this.rim-=0.02f;
+			yield return null;
+
+		}
+
+		yield return new WaitForSeconds (0.5f);
+
+	}
+
+	private void shakePiece(){
+		
+	}
+}
